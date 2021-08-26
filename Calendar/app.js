defd783b62269cc60c00924716a4ed9c6fe2d28d
Roman Kozhukhovskyi <4kroman@gmail.com>
2021-08-26 20:42:20 +0300

    code with Kate)

--- Calendar/app.js
+++ Calendar/app.js
@@ -1,4 +1,3 @@
-
 const MONTH_LIST = [
     'Январь',
     'Февраль',
@@ -14,18 +13,17 @@ const MONTH_LIST = [
     'Декабрь',
 ];
 
-const YEAR_LIST = function(fromYear, toYear) {
+const YEAR_LIST = function (fromYear, toYear) {
     let genArrYear = [];
-    for(let i = fromYear; i < toYear; i++) {
+    for (let i = fromYear; i < toYear; i++) {
         genArrYear.push(i);
     }
     return genArrYear;
 }(1970, 2050);
 
 class Calendar {
-    constructor(rootEl, monthList = [...MONTH_LIST],  yearList = [...YEAR_LIST]) {
+    constructor(rootEl, monthList = [...MONTH_LIST], yearList = [...YEAR_LIST]) {
 
-        const currentDay = new Date();
 
         this.yearList = yearList;
         this.monthList = monthList;
@@ -36,6 +34,7 @@ class Calendar {
         this.selectYear = rootEl.querySelector('.select-year');
         this.daysList = rootEl.querySelector('.cal-days');
 
+        const currentDay = new Date();
         this.state = this.getMonthState(currentDay.getFullYear(), currentDay.getMonth());
         this.render();
 
@@ -54,11 +53,16 @@ class Calendar {
     }
 
 
-
+    compareDatesWithoutTime(date1, date2) {
+        return date1.getDate() === date2.getDate() &&
+            date1.getMonth() === date2.getMonth() &&
+            date1.getFullYear() == date2.getFullYear();
+    }
 
     createDay(year, month, day) {
         return new Date(year, month, day, 12, 0, 0);
     }
+
     getMonthState(year, month) {
         // firstDayInMonth -- первый день месяца
         const firstDayInMonth = this.createDay(year, month, 1);
@@ -90,35 +94,65 @@ class Calendar {
         ) {
             days.push(new Date(currentDay));
         }
-
+        const today = new Date();
+        const todayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
         return {
             days,
             month: firstDayInMonth.getMonth(),
-            year: firstDayInMonth.getFullYear()
+            year: firstDayInMonth.getFullYear(),
+            today: todayDate,
+            selectedDate: todayDate,
         };
     }
+
     setMonth(month, year) {
         this.state = this.getMonthState(year, month);
+        console.log(this.state);
         this.render();
     }
+
     getSelectedMonth() {
-        this.setMonth(this.state.month = this.selectMonth.options.selectedIndex, this.state.year);
+        this.setMonth(this.selectMonth.options.selectedIndex, this.state.year);
     }
+
     getSelectedYear() {
-        this.setMonth(this.state.month, this.state.year = this.selectYear.value);
+        console.log(this.selectYear.value);
+        this.setMonth(this.state.month, this.selectYear.value);
     }
+
     nextMonth() {
         this.setMonth(this.state.month + 1, this.state.year);
     }
+
     prevMonth() {
         this.setMonth(this.state.month - 1, this.state.year);
     }
 
     setActiveDay(e) {
-        this.dayLinks = this.rootEl.querySelectorAll('.cal-day a');
-        this.dayLinks.forEach(a => a.classList.remove('active-day'));
-        e.target.classList.add('active-day');
-        console.log(e.target.getAttribute('aria-label'));
+        e.preventDefault();
+
+        const selectedDayEl = e.target;
+        if (!selectedDayEl) {
+            return;
+        }
+
+        const dayLink = this.rootEl.querySelector('.active-day');
+        if (dayLink) {
+            dayLink.classList.remove('active-day');
+        }
+
+        selectedDayEl.classList.add('active-day');
+
+        const selectedDate = new Date(selectedDayEl.getAttribute('aria-label'));
+
+        this.updateState(selectedDate);
+        console.log(this.state);
+    }
+
+    updateState(selectedDate) {
+        this.state.selectedDate = selectedDate;
+        this.state.year = selectedDate.getFullYear();
+        this.state.month = selectedDate.getMonth();
     }
 
     createDayLink(day) {
@@ -131,19 +165,23 @@ class Calendar {
         if (day.getMonth() !== this.state.month) {
             rootEl.classList.add('cal-day--not-in-month');
         }
-        // l.href = `?day=${day.toJSON().split('T')[0]}`;
+        l.href = `?day=${day.toJSON().split('T')[0]}`;
         l.innerText = day.getDate();
         l.setAttribute('aria-label', day.toDateString());
         l.classList.add('cal-dayLink');
 
         // находим сегодняшний день при рендере календаря
-        if (day.getDate() === new Date().getDate()) {
+        const today = new Date();
+        if (this.compareDatesWithoutTime(day, today)) {
+            // console.log(day);
+            // console.log(today);
             l.className = 'cal-dayLink';
             l.classList.add('now-day');
         }
 
         return rootEl;
     }
+
     createMonthSelect(month) {
 
         const optionEl = document.createElement('option');
@@ -156,6 +194,7 @@ class Calendar {
         }
         return optionEl;
     }
+
     createYearSelect(year) {
 
         const optionEl = document.createElement('option');
@@ -168,6 +207,7 @@ class Calendar {
         }
         return optionEl;
     }
+
     render() {
         const {days} = this.state;
         // console.log('this.state',this.state);
@@ -181,6 +221,7 @@ class Calendar {
         // console.log('dayLinks',this.dayLinks);
     }
 }
+
 const calendar = new Calendar(document.querySelector('.cal'));
 
 
