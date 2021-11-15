99221977cf007e398c38f7a6bb4c6c143383db05
Roman Kozhukhovskyi <4kroman@gmail.com>
2021-11-15 15:55:21 +0200

    fix bug in Safari

--- CV_Kozhukhovskyi_Roman_front-end/js/app.js
+++ CV_Kozhukhovskyi_Roman_front-end/js/app.js
@@ -1,21 +1,22 @@
 const progress = document.getElementsByTagName('PROGRESS');
 const arrValue = [];
 
-for (let i = 0; i < progress.length; i++) {
-   arrValue.push( +progress[i].getAttribute("value"));
-   progress[i].setAttribute("value", '0');
+for (let j = 0; j < progress.length; j++) {
+   arrValue.push( +progress[j].getAttribute("value"));
+   progress[j].setAttribute("value", '0');
 }
-
 for (let i = 0; i < progress.length; i++) {
-    let count = 0;
-    function changeProgress() {
-        if (count >= arrValue[i]) {
-            return;
-        }
-        count++;
-        progress[i].setAttribute("value", count.toString());
-        setTimeout(changeProgress, 40);
-    }
-    changeProgress();
+  let count = 0;
+  while (count <= arrValue[i]) {
+    task(count, i);
+    count++;
+  }
+
+  function task(count, i) {
+    setTimeout(function() {
+      progress[i].setAttribute("value", count.toString());
+      console.log(count);
+    }, 40 * count);
+  }
 }
 
