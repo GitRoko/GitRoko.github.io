d38725d72dada258e6a58efc72524c0bfedcb805
Roman Kozhukhovskyi <4kroman@gmail.com>
2021-11-15 16:08:45 +0200

    fix app.js

--- CV_Kozhukhovskyi_Roman_front-end/js/app.js
+++ CV_Kozhukhovskyi_Roman_front-end/js/app.js
@@ -2,22 +2,21 @@
 const progress = document.getElementsByTagName('PROGRESS');
 const arrValue = [];
 
-for (let j = 0; j < progress.length; j++) {
-   arrValue.push( +progress[j].getAttribute("value"));
-   progress[j].setAttribute("value", '0');
-}
 for (let i = 0; i < progress.length; i++) {
-  let count = 0;
-  while (count <= arrValue[i]) {
-    task(count, i);
-    count++;
-  }
-
-  function task(count, i) {
-    setTimeout(function() {
-      progress[i].setAttribute("value", count.toString());
-      console.log(count);
-    }, 40 * count);
-  }
+   arrValue.push( +progress[i].getAttribute("value"));
+   progress[i].setAttribute("value", '0');
 }
 
+for (let i = 0; i < progress.length; i++) {
+    let count = 0;
+    while (count <= arrValue[i]) {
+        task(count, i);
+        count++;
+    }
+
+    function task(count, i) {
+        setTimeout(function() {
+            progress[i].setAttribute("value", count.toString());
+        }, 40 * count);
+    }
+}
